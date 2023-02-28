27024106990db33dc47cdfd83d32f1565cb40844
wi2410 <101503219+wi2410@users.noreply.github.com>
2023-02-28 18:10:32 +0200

    Update NewsPage.jsx

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -4,7 +4,7 @@ import { Container, TitleNews } from './NewsPage.styled';
 import { useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectNewsSearchString} from '../../redux/news/selectors';
+import { selectNewsSearchString } from '../../redux/news/selectors';
 import { filterNews } from '../../redux/news/filterSlice';
 
 const NewsPage = () => {
@@ -29,18 +29,13 @@ const NewsPage = () => {
     setSearchParams(newParams);
   };
 
-  console.log(searchQuery.filter);
-  
   return (
     <Container>
       <TitleNews>News</TitleNews>
-      <NewsSearchForm onSubmit={setSearchQueryParam}/>
+      <NewsSearchForm onSubmit={setSearchQueryParam} />
       <NewsList />
     </Container>
-  
-
-  )
- 
+  );
 };
 
 export default NewsPage;
